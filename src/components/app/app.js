d8f89e18ccc78afc977915cf3b091858bfab8681
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-18 08:57:58 +0300

    Add files via upload

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 className="App">Собачки, следящие за тобой, пока ты изучаешь React</h1>
+      </header>
+    );
+  }
+}
+
+export default App;
