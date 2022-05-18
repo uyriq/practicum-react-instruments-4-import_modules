1f3eea7b12e720f1b40818638884e912a4d10ca1
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-18 09:39:49 +0300

    export default class App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../header/header'
 import Main from '../main/main'
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
      <div>
@@ -12,5 +12,3 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
